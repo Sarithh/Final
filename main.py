8a6e69ae71c82aaa86b448d1ebc68437c4ccd9bd
Sarithh <80663021+Sarithh@users.noreply.github.com>
2023-03-27 11:51:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from routes.courses_routes import course_api_router
+from routes.expresses_routes import express_api_router
 
 app = FastAPI()
 
-app.include_router(course_api_router)
+app.include_router(express_api_router)
